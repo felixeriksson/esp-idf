08aa4702b67e168380670522937d2e16a9af3d9d
Felix Eriksson <felix.eriksson@revibeenergy.com>
2021-05-06 19:12:06 +0200

    A beginning towards merging cam_task and http-put function. Successfully (seemingly) takes pic but does not manage to perform PUT

--- examples/protocols/esp_http_client/main/esp_http_client_example.c
+++ examples/protocols/esp_http_client/main/esp_http_client_example.c
@@ -48,88 +48,6 @@ static const char *TAG = "HTTP_CLIENT";
 #define CAM_SCL   7
 #define CAM_SDA   8
 
-static void cam_task(void *arg)
-{    cam_config_t cam_config = {
-        .bit_width    = 8,
-        .mode.jpeg    = 1,
-        .xclk_fre     = 16 * 1000 * 1000,
-        .pin  = {
-            .xclk     = CAM_XCLK,
-            .pclk     = CAM_PCLK,
-            .vsync    = CAM_VSYNC,
-            .hsync    = CAM_HSYNC,
-        },
-        .pin_data     = {CAM_D0, CAM_D1, CAM_D2, CAM_D3, CAM_D4, CAM_D5, CAM_D6, CAM_D7},
-        .vsync_invert = true,
-        .hsync_invert = false,
-        .size = {
-            .width    = CAM_WIDTH,
-            .high     = CAM_HIGH,
-        },
-        .max_buffer_size = 8 * 1024,
-        .task_stack      = 1024,
-        .task_pri        = configMAX_PRIORITIES
-    };
-
-    /*!< With PingPang buffers, the frame rate is higher, or you can use a separate buffer to save memory */
-    cam_config.frame1_buffer = (uint8_t *)heap_caps_malloc(CAM_WIDTH * CAM_HIGH * 2 * sizeof(uint8_t), MALLOC_CAP_SPIRAM);
-    cam_config.frame2_buffer = (uint8_t *)heap_caps_malloc(CAM_WIDTH * CAM_HIGH * 2 * sizeof(uint8_t), MALLOC_CAP_SPIRAM);
-
-    cam_init(&cam_config);
-
-    sensor_t sensor;
-    int camera_version = 0;      /*!<If the camera version is determined, it can be set to manual mode */
-    SCCB_Init(CAM_SDA, CAM_SCL);
-    sensor.slv_addr = SCCB_Probe();
-    ESP_LOGI(TAG, "sensor_id: 0x%x\n", sensor.slv_addr);
-
-    camera_version = 2640;
-    if (sensor.slv_addr == 0x30 || camera_version == 2640) { /*!< Camera: OV2640 */
-        ESP_LOGI(TAG, "OV2640 init start...");
-
-        if (OV2640_Init(0, 1) != 0) {
-            goto fail;
-        }
-
-        if (cam_config.mode.jpeg) {
-            OV2640_JPEG_Mode();
-        } else {
-            OV2640_RGB565_Mode(false);	/*!< RGB565 mode */
-        }
-
-        OV2640_ImageSize_Set(800, 600);
-        OV2640_ImageWin_Set(0, 0, 800, 600);
-        OV2640_OutSize_Set(CAM_WIDTH, CAM_HIGH);
-    }
-
-    ESP_LOGI(TAG, "camera init done\n");
-    cam_start();
-
-    while (1) {
-        uint8_t *cam_buf = NULL;
-        cam_take(&cam_buf);
-        int w, h;
-        uint8_t *img = jpeg_decode(cam_buf, &w, &h);
-
-        if (img) {
-            ESP_LOGI(TAG, "jpeg: w: %d, h: %d\n", w, h);
-            // lcd_set_index(0, 0, w - 1, h - 1);
-            // lcd_write_data(img, w * h * sizeof(uint16_t));
-            free(img);
-        }
-        cam_give(cam_buf);
-        /*!< Use a logic analyzer to observe the frame rate */
-        vTaskDelay(20000/portTICK_PERIOD_MS);
-    }
-
-
-fail:
-    free(cam_config.frame1_buffer);
-    free(cam_config.frame2_buffer);
-    cam_deinit();
-    vTaskDelete(NULL);
-}
-
 /* Root cert for howsmyssl.com, taken from howsmyssl_com_root_cert.pem
 
    The PEM file was extracted from the output of this command:
@@ -213,7 +131,7 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
     return ESP_OK;
 }
 
-static void http_rest_with_url(void)
+static void http_rest_with_url(const char *put_data)
 {
     char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER] = {0};
     /**
@@ -224,7 +142,7 @@ static void http_rest_with_url(void)
      * If URL as well as host and path parameters are specified, values of host and path will be considered.
      */
     esp_http_client_config_t config = {
-        .url = "https://greenwatch-photos.s3.amazonaws.com/greenwatch_alpha1620244469.954609?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIARBRC4UMDHWWENT46%2F20210505%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Date=20210505T195429Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEJT%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCmV1LW5vcnRoLTEiRjBEAiBtL%2F4H6UhVuDby0Nsr1bHolorME4KHb7c57U%2BWI0Df2wIge24kgVgHCYjgy5vDzBYVE6wwK3rzTWOXDP5YqtpY9aUq3AEIHRAAGgwwNzIwMTM4ODIxMTgiDCxmHbT82GUXLwrgOSq5ART0rM1htW%2FohyN%2BgCs3Z5ilHiAK2BUoSu2XHsz7Dh4upPCKvl3K7bofh6TSQU3FI2alSHH99%2BqFX7bs6ZhUS3Qs3pVXu4l%2FhsfodoO4dU224Hl7bhEgraGFciSMvVm97Oz6DF%2F6Cdin9tYpzB2LydlR3MUWGzagj3dLV6vBHqgddYXUUmiwLGggAQ%2BYv9W7z5zgwanN%2BAFNDPe868UgoYqa%2Bv8GWDKnY%2BKz9JAnkLJuRm0IwMLykhx8MPTvy4QGOuEB3%2FjdasO%2B27kSes10JWyfkCvAlQORylA5j5sMFxBZ9SoRvw4uZDEJCG83i%2FE3Ddvk35ph0EQ433O2jQTSfTqfMf99d6TdXepUzXi84YVaXFTU%2BlBWtm6jfpcTTjC0Wx1G8%2BCRt5zi7rpVEGpYl61hH3EQwEwuBOFTVOtp5yxyxOZ5frCCv3SRgYADDuBREO6oquovcsdxA9CucKAcnIup3NtZtPbDBIJU1TYwtoeFgVbGRCHJP%2BbkyvTXLD3K4OSUT2zWQJjWCDpPObqqgmnuZzqvQh0RANuDEMTSou7x5gy0&X-Amz-Signature=d63cb9864fbf8f415313615ba2e5c49e4ebb9a704b31a9275c62274a1b37824f",
+        .url = "https://greenwatch-photos.s3.amazonaws.com/greenwatch_alpha1620320029.193127?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIARBRC4UMDIYH72XF3%2F20210506%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Date=20210506T165349Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEKn%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCmV1LW5vcnRoLTEiRzBFAiBTE3mhPAOuNzcwgB7P5Zp%2B%2F3WVRWcYNxs%2BxYd%2BOixlWgIhAIn7V3MKbhm8Oxz%2FjuGadwK8tOp1DGlBFv%2B6WEgGWmLNKqICCDIQABoMMDcyMDEzODgyMTE4IgxvkSJObM640uwzhigq%2FwFq0dXcvBQ8e3XIGFcOBtVJO3akk3bZoPKE%2BVOP571qDE4n5PVcP0oRLzyRb2xSW4q%2Bm1y4RqHn83tkVJ1aZndsgMrNeapsugBNQio8tXc3S1dUxVjz8bo0RGHdvv5j2%2BhoNVv2CYIoct6Cru8tzRq0n1%2FChPfTSpjCNtaS9JkZRY9spuyHQP1i7MzSc6o6irQNBbMqh4LKujTsxYYtR7qEuszQ8ISzgqsZG1Czda6FpyCuJSZqUbeQSNjT74p76mKh2yFShkjck1lwS0NFkSL2bivaWbVd1tgp9UPi4sQm1Z%2FL94uSaBDA%2FY7orFhndB3SWROzLzU2h2a%2BWeHLgkgwm77QhAY6mgEtUFDUCW13VeZZz2JkDbVPfENm20OoSfa6CdPIS7g%2Fq8pKmFx4DWgOWrZ7AygoBuwRhR5BK6EI4zUM%2Bzg4sGj8Xourndmu7ZzJh%2BBkGxwH8g4N%2F%2BtKL4Q93TOBULJrCkQzK30PTvy2TLij41i6vf%2Fv0WnwCXr%2F13Huhsega8UgFBChA6uiufelsz8cV81ZLjcxdAxx1DQfeQjx&X-Amz-Signature=914a1ff5988fb12f15899122a6e9ae17403992cae9d9c60e28840ec5080d2be5",
         .event_handler = _http_event_handler,
         .user_data = local_response_buffer,        // Pass address of local buffer to get response
         .buffer_size_tx = 2048,
@@ -232,7 +150,6 @@ static void http_rest_with_url(void)
     esp_http_client_handle_t client = esp_http_client_init(&config);
 
     //PUT
-    const char *put_data = "Using a Palantír requires a person with great strength of will and wisdom. The Palantíri were meant to ";
     esp_http_client_set_method(client, HTTP_METHOD_PUT);
     esp_http_client_set_header(client, "Content-Type", "image/jpeg");
     esp_http_client_set_post_field(client, put_data, strlen(put_data));
@@ -248,9 +165,94 @@ static void http_rest_with_url(void)
     esp_http_client_cleanup(client);
 }
 
+static void cam_task(void *arg)
+{    cam_config_t cam_config = {
+        .bit_width    = 8,
+        .mode.jpeg    = 1,
+        .xclk_fre     = 16 * 1000 * 1000,
+        .pin  = {
+            .xclk     = CAM_XCLK,
+            .pclk     = CAM_PCLK,
+            .vsync    = CAM_VSYNC,
+            .hsync    = CAM_HSYNC,
+        },
+        .pin_data     = {CAM_D0, CAM_D1, CAM_D2, CAM_D3, CAM_D4, CAM_D5, CAM_D6, CAM_D7},
+        .vsync_invert = true,
+        .hsync_invert = false,
+        .size = {
+            .width    = CAM_WIDTH,
+            .high     = CAM_HIGH,
+        },
+        .max_buffer_size = 8 * 1024,
+        .task_stack      = 1024,
+        .task_pri        = configMAX_PRIORITIES
+    };
+
+    /*!< With PingPang buffers, the frame rate is higher, or you can use a separate buffer to save memory */
+    cam_config.frame1_buffer = (uint8_t *)heap_caps_malloc(CAM_WIDTH * CAM_HIGH * 2 * sizeof(uint8_t), MALLOC_CAP_SPIRAM);
+    cam_config.frame2_buffer = (uint8_t *)heap_caps_malloc(CAM_WIDTH * CAM_HIGH * 2 * sizeof(uint8_t), MALLOC_CAP_SPIRAM);
+
+    cam_init(&cam_config);
+
+    sensor_t sensor;
+    int camera_version = 0;      /*!<If the camera version is determined, it can be set to manual mode */
+    SCCB_Init(CAM_SDA, CAM_SCL);
+    sensor.slv_addr = SCCB_Probe();
+    ESP_LOGI(TAG, "sensor_id: 0x%x\n", sensor.slv_addr);
+
+    camera_version = 2640;
+    if (sensor.slv_addr == 0x30 || camera_version == 2640) { /*!< Camera: OV2640 */
+        ESP_LOGI(TAG, "OV2640 init start...");
+
+        if (OV2640_Init(0, 1) != 0) {
+            goto fail;
+        }
+
+        if (cam_config.mode.jpeg) {
+            OV2640_JPEG_Mode();
+        } else {
+            OV2640_RGB565_Mode(false);	/*!< RGB565 mode */
+        }
+
+        OV2640_ImageSize_Set(800, 600);
+        OV2640_ImageWin_Set(0, 0, 800, 600);
+        OV2640_OutSize_Set(CAM_WIDTH, CAM_HIGH);
+    }
+
+    ESP_LOGI(TAG, "camera init done\n");
+    cam_start();
+
+    while (1) {
+        uint8_t *cam_buf = NULL;
+        cam_take(&cam_buf);
+        int w, h;
+        uint8_t *img = jpeg_decode(cam_buf, &w, &h);
+
+        if (img) {
+            ESP_LOGI(TAG, "jpeg: w: %d, h: %d\n", w, h);
+            // lcd_set_index(0, 0, w - 1, h - 1);
+            // lcd_write_data(img, w * h * sizeof(uint16_t));
+            const char *put_data = "BlubbUsing a Palantír requires a person with great strength of will and wisdom. The Palantíri were meant to ";
+            http_rest_with_url(put_data);
+            free(img);
+        }
+        cam_give(cam_buf);
+        /*!< Use a logic analyzer to observe the frame rate */
+        vTaskDelay(20000/portTICK_PERIOD_MS);
+    }
+
+
+fail:
+    free(cam_config.frame1_buffer);
+    free(cam_config.frame2_buffer);
+    cam_deinit();
+    vTaskDelete(NULL);
+}
+
 static void http_test_task(void *pvParameters)
 {
-    http_rest_with_url();
+    const char *put_data = "BlubbUsing a Palantír requires a person with great strength of will and wisdom. The Palantíri were meant to ";
+    http_rest_with_url(put_data);
     ESP_LOGI(TAG, "Finish http example");
     vTaskDelete(NULL);
 }
@@ -272,7 +274,8 @@ void app_main(void)
      */
     ESP_ERROR_CHECK(example_connect());
     ESP_LOGI(TAG, "Connected to AP, begin http example");
-    xTaskCreate(cam_task, "cam_task", 2048, NULL, 6, NULL);
+    xTaskCreate(cam_task, "cam_task", 2048+8192, NULL, 6, NULL);
+    //http_rest_with_url();
 
-    xTaskCreate(&http_test_task, "http_test_task", 8192, NULL, 5, NULL);
+    //xTaskCreate(&http_test_task, "http_test_task", 8192, NULL, 5, NULL);
 }
